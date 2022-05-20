725c2764aa59ceb9f6234d73147e5ad5e44f8566
Kevink12341 <kevin.kuiper@gmail.com>
2022-05-20 12:37:11 +0200

    commit on test

--- Fetch_data/Fetch_data.ts
+++ Fetch_data/Fetch_data.ts
@@ -1,23 +1,17 @@
-import fetch from 'node-fetch';
+import fetch,{RequestInit,Headers} from 'node-fetch';
 import {cbs_links} from './Links_and_cleaning.js'
-import {xml_text_parser} from './Parse_XML_input.js'
-
-interface request {
-    method: any,
-    headers: any,
-    redirect: any,
-}
+import {xml_text_parser} from './Parse_XML_inputs.js'
 
 var myHeaders = new Headers();
 myHeaders.append("Accept", "text")
 
-var requestOptions:request = {
+var requestOptions:RequestInit = {
   method: 'GET',
   headers: myHeaders,
   redirect: 'follow',
 };
 
-export const import_schema = () => {
+export const import_cbs_xml = async () => {
     return fetch(cbs_links.metadata, requestOptions)
        .then((response:any) => response.text())
             .then( (text:any) => { return xml_text_parser(text); }
@@ -25,7 +19,7 @@ export const import_schema = () => {
 
 export const import_data = () => {    
     return fetch(cbs_links.observations).then((response:any) => { 
-        return response.json().then((json: any) => {
+        return response.json().then((json: any) =>  {
             let data = json.value;
             return data;
         })
