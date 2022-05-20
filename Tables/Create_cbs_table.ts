f99c3afd4de34641cc96f91fb3c261b3238d4aad
Kevink12341 <kevin.kuiper@gmail.com>
2022-05-20 12:39:16 +0200

    commit on Test

---
+++ Tables/Create_cbs_table.ts
@@ -0,0 +1,9 @@
+import {Connections} from '../DB_architecture/DB_connection';
+
+const IDtypes = {
+    "Edm.Int64": "INT(64)", 
+    "Edm.String": "VARCHAR(255)",
+    "Edm.Double": "DOUBLE" ,
+    "Edm.Int32": "INT(32)",
+    "Edm.Boolean": "BOOLEAN"
+  };
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 import { create_DB } from "./DB_architecture/Create_database.js";
 import { check_cbs_tables } from "./DB_architecture/Create_CBS_Tables.js";
+import { import_cbs_xml } from "./Fetch_data/Fetch_data.js";
 // First the code generates a DB by running create_db from create_database.js should none exist.
 create_DB();
 // When it finds that the DB exists it creates CBS_Tables for the DB architecture.
 check_cbs_tables();
+// Async get XML from CBS.nl
+import_cbs_xml();
 //# sourceMappingURL=index.js.map
