3707ef928f6ffb361ffe6754be5283918bf417d9
Kevink12341 <kevin.kuiper@gmail.com>
2022-06-01 09:57:13 +0200

    it works

--- dist/DB_architecture/random_input.js
+++
@@ -1,4 +0,0 @@
-//to be updated for random inputs
-// Temporary value for creating a databasename. Will have to be added in the front-end to ask the user to define the DB.
-export const databasename = "cbsdata";
-//# sourceMappingURL=random_input.js.map
